8c42a1b6bea4a5269d1caa41c7a7084a5613c7fd
tackyu <tack970913@gmail.com>
2024-03-31 02:19:08 +0900

    feat : chess_game, chessboard 테이블 생성

---
+++ src/main/createTable.sql
@@ -0,0 +1,13 @@
+CREATE table chess_game(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    state varchar(20),
+    PRIMARY KEY (id)
+);
+
+CREATE table chessboard(
+    chess_game_id BIGINT,
+    position varchar(2),
+    piece varchar(20)
+
+);
+
