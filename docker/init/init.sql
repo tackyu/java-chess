0ebb30fc7048dae405fed9b991403a5a9a990ee5
tackyu <tack970913@gmail.com>
2024-04-04 15:00:28 +0900

    docs : DB 실행 방법 명시

---
+++ docker/init/init.sql
@@ -0,0 +1,16 @@
+use chess;
+
+CREATE table chess_game
+(
+    id    BIGINT NOT NULL AUTO_INCREMENT,
+    state varchar(20),
+    PRIMARY KEY (id)
+);
+
+CREATE table chessboard
+(
+    chess_game_id BIGINT,
+    position      varchar(2),
+    piece         varchar(20)
+);
+
